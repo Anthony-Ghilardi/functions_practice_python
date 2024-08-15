bb0c45e970d1430da42a118369454cd0b68f6531
Anthony-Ghilardi <Ghilardi@unlv.nevada.edu>
2024-08-15 10:47:32 -0700

    Completed

---
+++ functions_practice.py
@@ -0,0 +1,38 @@
+# FUNCTION 1
+
+# def hello():
+#     print("Greetings user")
+
+# hello()
+
+
+
+#----------------------------------------------------------------------------------------------------
+
+
+
+#FUNCTION 2
+
+# def pack(sandwich, granola_bar, energy_drink):
+#     my_lunch_list = [sandwich, granola_bar, energy_drink]
+#     return my_lunch_list
+
+# print(pack("Ham and cheese sandwich", "Peanut butter bar", "Monster"))
+
+
+
+#----------------------------------------------------------------------------------------------------
+
+
+
+# FUNCTION 3
+
+# def eat_lunch(my_lunch):
+#     if my_lunch == []:
+#         print("My lunchbox is empty")
+#     else:
+#         print("First I eat", my_lunch[0])
+#         for i in my_lunch[1:]:
+#             print("Next i eat", i)
+
+# eat_lunch(["Ham and cheese sandwich", "Peanut butter bar", "Trail mix"])
